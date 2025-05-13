dbe98d5952241123d5a23695933c9defef462170
Amerkhan786ak <khanameer2646@gmail.com>
2025-05-13 15:33:30 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,15 @@
+//function.c
+# include <stdio.h>
+int c;
+int add(int a,int b){// function defination
+c=a+b;
+https://www.programiz.com/return c;
+}
+int main()
+{
+int result=add(6,5);
+printf("%d",c);
+return 0;
+}
+
+
